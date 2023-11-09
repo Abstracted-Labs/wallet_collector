6b55f648cb544175e1dd95602cd5b41861045c94
Gabriel Facco de Arruda <arrudagates@gmail.com>
2023-11-09 11:55:44 -0300

    Change wallet collector mode database

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 #![allow(clippy::too_many_arguments)]
 
+use codec::{Decode, Encode};
 #[cfg(feature = "collector")]
 use hex::ToHex;
-#[cfg(feature = "collector")]
-use rusoto_core::Region;
-#[cfg(feature = "collector")]
-use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient};
 use serenity::{
     async_trait,
     model::{
@@ -17,15 +14,18 @@ use serenity::{
             },
             Interaction, InteractionApplicationCommandCallbackDataFlags, InteractionResponseType,
         },
+        prelude::AttachmentType,
     },
     prelude::*,
 };
 use sp_core::{
-    crypto::{AccountId32, Ss58Codec},
+    crypto::{AccountId32, Ss58AddressFormat, Ss58Codec},
     Pair,
 };
+use std::borrow::Cow;
 #[cfg(feature = "collector")]
 use std::collections::HashMap;
+#[cfg(feature = "funder")]
 use subxt::{tx::PairSigner, OnlineClient, PolkadotConfig};
 
 struct Handler;
@@ -38,7 +38,11 @@ pub mod chain {}
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
-            let content: Result<String, String> = match command.data.name.as_str() {
+            let content: Result<(String, Option<Vec<u8>>), String> = match command
+                .data
+                .name
+                .as_str()
+            {
                 #[cfg(feature = "collector")]
                 "give_wallet" => {
                     let options = command
@@ -51,131 +55,75 @@ impl EventHandler for Handler {
                         .expect("Expected user object");
 
                     if let ApplicationCommandInteractionDataOptionValue::String(string) = options {
-                        if let Ok(accountid) = sp_core::crypto::AccountId32::from_ss58check(string)
+                        if let Ok(account_id) = sp_core::crypto::AccountId32::from_ss58check(string)
                         {
-                            let db_client = ctx.data.write().await;
-                            let db_client = db_client.get::<DbData>().unwrap();
+                            let data = ctx.data.write().await;
+                            if let Some((db, _, _)) = data.get::<DbData>() {
+                                let discord_id = command.user.id.to_string().encode();
+                                let discord_username = command.user.name.clone();
 
-                            let result = db_client
-                                .put_item(rusoto_dynamodb::PutItemInput {
-                                    condition_expression: Some(String::from(
-                                        "attribute_not_exists",
-                                    )),
-                                    conditional_operator: None,
-                                    expected: None,
-                                    expression_attribute_names: None,
-                                    expression_attribute_values: None,
-                                    item: HashMap::from_iter(
-                                        vec![(
-                                            command.user.id.to_string(),
-                                            AttributeValue {
-                                                b: None,
-                                                bool: None,
-                                                bs: None,
-                                                l: None,
-                                                m: None,
-                                                n: None,
-                                                ns: None,
-                                                null: None,
-                                                s: Some(accountid.encode_hex::<String>()),
-                                                ss: None,
-                                            },
-                                        )]
-                                        .into_iter(),
-                                    ),
-                                    return_consumed_capacity: None,
-                                    return_item_collection_metrics: None,
-                                    return_values: None,
-                                    table_name: String::from("airdrops"),
-                                })
-                                .await;
-
-                            if let Err(error) = result {
-                                println!("Error: {:?}", error);
-                                match error {
-                                    rusoto_core::RusotoError::Service(
-                                        rusoto_dynamodb::PutItemError::ConditionalCheckFailed(_),
-                                    ) => Ok(String::from("Your account is already in the list.")),
-
-                                    _ => Ok(String::from("An error has occurred")),
+                                if let Ok(_) =
+                                    db.insert(discord_id, (discord_username, account_id).encode())
+                                {
+                                    Ok((String::from("Account registered successfully!"), None))
+                                } else {
+                                    Ok((String::from("Error registering account."), None))
                                 }
                             } else {
-                                Ok(String::from("Address added succesfully!"))
+                                Ok((String::from("Error opening address database."), None))
                             }
                         } else {
-                            Ok(String::from("Please provide a valid address."))
+                            Ok((String::from("Invalid address provided."), None))
                         }
                     } else {
-                        Ok(String::from("Please provide a valid address."))
+                        Ok((String::from("No address provided."), None))
                     }
                 }
 
                 #[cfg(feature = "collector")]
-                "replace_wallet" => {
-                    let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected user option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected user object");
-
-                    if let ApplicationCommandInteractionDataOptionValue::String(string) = options {
-                        if let Ok(accountid) = sp_core::crypto::AccountId32::from_ss58check(string)
+                "generate_csv" => {
+                    let data = ctx.data.write().await;
+                    if let Some((db, guild_id, admin_role_id)) = data.get::<DbData>() {
+                        if let Ok(true) = command
+                            .user
+                            .has_role(ctx.http.clone(), *guild_id, *admin_role_id)
+                            .await
                         {
-                            let db_client = ctx.data.write().await;
-                            let db_client = db_client.get::<DbData>().unwrap();
+                            let mut wtr = csv::Writer::from_writer(vec![]);
 
-                            let result = db_client
-                                .put_item(rusoto_dynamodb::PutItemInput {
-                                    condition_expression: Some(String::from("attribute_exists")),
-                                    conditional_operator: None,
-                                    expected: None,
-                                    expression_attribute_names: None,
-                                    expression_attribute_values: None,
-                                    item: HashMap::from_iter(
-                                        vec![(
-                                            command.user.id.to_string(),
-                                            AttributeValue {
-                                                b: None,
-                                                bool: None,
-                                                bs: None,
-                                                l: None,
-                                                m: None,
-                                                n: None,
-                                                ns: None,
-                                                null: None,
-                                                s: Some(accountid.encode_hex::<String>()),
-                                                ss: None,
-                                            },
-                                        )]
-                                        .into_iter(),
-                                    ),
-                                    return_consumed_capacity: None,
-                                    return_item_collection_metrics: None,
-                                    return_values: None,
-                                    table_name: String::from("airdrops"),
-                                })
-                                .await;
-
-                            if let Err(error) = result {
-                                println!("Error: {:?}", error);
-                                match error {
-                                    rusoto_core::RusotoError::Service(
-                                        rusoto_dynamodb::PutItemError::ConditionalCheckFailed(_),
-                                    ) => Ok(String::from("Your account is not in the list.")),
-
-                                    _ => Ok(String::from("An error has occurred")),
+                            db.into_iter().for_each(|res| {
+                                if let Ok((key, value)) = res {
+                                    if let (Ok(discord_id), Ok((discord_username, account_id))) = (
+                                        String::decode(&mut key.to_vec().as_slice()),
+                                        <(String, AccountId32)>::decode(
+                                            &mut value.to_vec().as_slice(),
+                                        ),
+                                    ) {
+                                        wtr.write_record(&[
+                                            discord_id,
+                                            discord_username,
+                                            account_id.to_ss58check_with_version(
+                                                Ss58AddressFormat::custom(2),
+                                            ),
+                                        ])
+                                        .unwrap();
+                                    };
                                 }
+                            });
+
+                            if let Ok(sheet) = wtr.into_inner() {
+                                Ok((String::from("addresses.csv"), Some(sheet)))
                             } else {
-                                Ok(String::from("Address added succesfully!"))
+                                Ok((String::from("Failed to write csv."), None))
                             }
                         } else {
-                            Ok(String::from("Please provide a valid address."))
+                            Ok((
+                                String::from("This command is onlyavailable to admins."),
+                                None,
+                            ))
                         }
                     } else {
-                        Ok(String::from("Please provide a valid address."))
+                        Ok((String::from("Error opening address database."), None))
                     }
                 }
 
@@ -225,7 +173,7 @@ impl EventHandler for Handler {
                     }
                 }
 
-                _ => Ok(String::from("Not implemented.")),
+                _ => Ok((String::from("Not implemented."), None)),
             };
 
             if let Err(why) = command
@@ -233,22 +181,27 @@ impl EventHandler for Handler {
                     response
                         .kind(InteractionResponseType::ChannelMessageWithSource)
                         .interaction_response_data(|message| {
-                            message
+                            let m = message
                                 .content(match content.clone() {
-                                    Ok(msg) => msg,
+                                    Ok((msg, _)) => msg,
                                     Err(post_info) => post_info,
                                 })
-                                .flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL)
+                                .flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+
+                            if let Ok((filename, Some(file_data))) = content.clone() {
+                                m.add_file(AttachmentType::Bytes {
+                                    data: Cow::Owned(file_data),
+                                    filename,
+                                });
+                            }
+
+                            m
                         })
                 })
                 .await
             {
                 println!("Cannot respond to slash command: {}", why);
             }
-
-            //  if let Err(why) = content {
-            //      panic!("Called panic on a restart-required error: '{}'", why);
-            //  }
         }
     }
 
@@ -274,15 +227,8 @@ impl EventHandler for Handler {
         #[cfg(feature = "collector")]
         ApplicationCommand::create_global_application_command(&ctx.http, |command| {
             command
-                .name("replace_wallet")
-                .description("Replace the wallet you gave to the bot")
-                .create_option(|option| {
-                    option
-                        .name("wallet")
-                        .description("The actual wallet")
-                        .kind(ApplicationCommandOptionType::String)
-                        .required(true)
-                })
+                .name("generate_csv")
+                .description("Generate CSV of the registered addresses")
         })
         .await
         .unwrap();
@@ -305,6 +251,7 @@ impl EventHandler for Handler {
     }
 }
 
+#[cfg(feature = "funder")]
 async fn handle_funding_command(
     api: &OnlineClient<PolkadotConfig>,
     pair_signer: &PairSigner<PolkadotConfig, sp_core::sr25519::Pair>,
@@ -339,7 +286,7 @@ struct DbData;
 
 impl TypeMapKey for DbData {
     #[cfg(feature = "collector")]
-    type Value = DynamoDbClient;
+    type Value = (sled::Db, u64, u64);
     #[cfg(feature = "funder")]
     type Value = (
         OnlineClient<PolkadotConfig>,
@@ -357,18 +304,21 @@ async fn main() {
 
     let application_id: u64 = dotenv::var("app_id").unwrap().parse().unwrap();
 
-    let mut client = Client::builder(token)
+    let mut client = Client::builder(token, GatewayIntents::default())
         .event_handler(Handler)
         .application_id(application_id)
         .await
         .expect("Error creating client");
 
     #[cfg(feature = "collector")]
-    {
-        let db_client = DynamoDbClient::new(Region::UsEast1);
-        let mut data = client.data.write().await;
-        data.insert::<DbData>(db_client);
-    }
+    client.data.write().await.insert::<DbData>((
+        sled::open("wallet_db").unwrap(),
+        dotenv::var("server_id").unwrap().parse::<u64>().unwrap(),
+        dotenv::var("admin_role_id")
+            .unwrap()
+            .parse::<u64>()
+            .unwrap(),
+    ));
 
     #[cfg(feature = "funder")]
     client.data.write().await.insert::<DbData>((
